26538e7b46bbb15fd7294426ff7fb98ee304285f
kashif74 <kashif.alig@gmail.com>
2015-04-24 15:25:32 +0100

    Add documentation

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,21 +5,22 @@
 # === Parameters
 #
 # Document parameters here.
+# Parameters are supposed to pass directly to sub-class through hiera. Defaults are provided for all parameters. 
+# $cvmfs_http_proxy should be changed to site squid server. 
+
+# ===List of all variables with default value which can be overwritten from hiera
+#     cvmfs_simple::repo::cvmfs_yum: 'http://cern.ch/cvmrepo/yum/cvmfs/EL/${::operatingsystemmajrelease}/${::architecture}'
+#     cvmfs_simple::repo::cvmfs_yum_testing: 'http://cern.ch/cvmrepo/yum/cvmfs-testing/EL/${::operatingsystemmajrelease}/${::architecture}'
+#     cvmfs_simple::repo::cvmfs_yum_testing_enabled: 'false'
+#     cvmfs_simple::install::cvmfs_version: 'latest'
+#     cvmfs_simple::config::lenspath: '/var/lib/puppet/lib/augeas/lenses'
+#     cvmfs_simple::config::cvmfs_quota_limit: '20000'
+#     cvmfs_simple::config::cvmfs_http_proxy: 'http://mysquidserver.example.com'
+#     cvmfs_simple::config::cvmfs_cache_base: '/var/cache/cvmfs2'
+#     cvmfs_simple::extra::repo: ['gridpp']
 #
-# [*sample_parameter*]
-#   Explanation of what this parameter affects and what it defaults to.
-#   e.g. "Specify one or more upstream ntp servers as an array."
-#
-# === Variables
-#
-# Here you should define a list of variables that this module would require.
-#
-# [*sample_variable*]
-#   Explanation of how this variable affects the funtion of this class and if
-#   it has a default. e.g. "The parameter enc_ntp_servers must be set by the
-#   External Node Classifier as a comma separated list of hostnames." (Note,
-#   global variables should be avoided in favor of class parameters as
-#   of Puppet 2.6.)
+# 
+# 
 #
 # === Examples
 #
