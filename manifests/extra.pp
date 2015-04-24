1d1b170feb355a2352347f37a8f9283f89ba93ea
kashif74 <kashif.alig@gmail.com>
2015-04-24 15:25:06 +0100

    remove oxford repo from default

--- manifests/extra.pp
+++ manifests/extra.pp
@@ -1,4 +1,4 @@
-class cvmfs_simple::extra( $repo = ['gridpp', 'oxford'] )   {
+class cvmfs_simple::extra( $repo = ['gridpp'] )   {
  cvmfs_simple::extra::add_extra_repo { $repo: } 
  define cvmfs_simple::extra::add_extra_repo ( $reponame = $title) {
     include "cvmfs_simple::$reponame"
